1f21f1336f8f59ca90d889d8fffab03ee575452d
Pan Cheng <pchengeducation@hotmail.com>
2018-07-27 00:47:36 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -8,6 +8,17 @@ weight:, handed:, complexion:, t_shirt_size:,
 wrist_size:, glove_size:, pant_length:, pant_width: )
     @name = name
     @birthday = birthday
+    @brown = Brown
+    @eye_color = eye_color
+    @height = height
+    @weight = weight
+    @handed = handed
+    @complexion = complexion
+    @t_shirt_size = t_shirt_size
+    @writst_size = wrist_size
+    @glove_size = glove_size
+    @pant_length = pant_length
+    @pant_width = pant_width
   end
   def get_married(person)
     self.partner = person
